4251961cbd80e097b02f1079d0c60191630fb775
Jerzy Pawlikowski <jurekpawlikowski@gmail.com>
2017-04-12 18:22:21 +0200

    fix README

--- README.rst
+++ README.rst
@@ -22,6 +22,9 @@ Usage
 -----
 
     from ant_colony.graph import Node, Graph
+
     nodes = [Node(0, 0), Node(0, 1), Node(1, 1), Node(1, 0)]
+
     graph = Graph(nodes)
+
     path, distance = graph.find_shortest_path()
--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+
+def read_readme():
+    with open("README.rst") as file:
+        return file.read()
+
+
+def read_license():
+    with open("LICENSE") as file:
+        return file.read()
+
 setup(
     name="ant_colony",
     author="Jerzy Pawlikowski",
     author_email="me@jerzypawlikowski.pl",
-    version="0.1.0",
+    version="0.1.2",
     url="https://github.com/jurekpawlikowski/ant-colony",
     package_dir={"ant_colony": "ant_colony"},
     packages=["ant_colony"],
-    license=open("LICENSE").read(),
+    license=read_license(),
     description="Implementation of the Ant Colony system",
-    long_description=open("README.rst").read()
+    long_description=read_readme()
 )
